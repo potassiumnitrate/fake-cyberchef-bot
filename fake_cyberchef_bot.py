a73095b28b3a4dde7e895edf52321fa179420e4d
potassiumnitrate <berlynn_neo@hotmail.com>
2021-07-28 00:21:10 +0800

    Edit bot file

--- fake_cyberchef_bot.py
+++ fake_cyberchef_bot.py
@@ -3,16 +3,16 @@ from flask import Flask, request
 from telebot.credentials import token, bot_username
 '''
 # initialize bot ---------------------------------------------------------------------------------------------------------------------------------------------------
-# import everything 
-import telegram
+# import everything
+#import telegram
 import telebot
 import logging
 from uuid import uuid4
 #from telebot import types
 from datetime import datetime
 #from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, ParseMode, InputTextMessageContent
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, InlineQueryHandler
-from telegram.utils.helpers import escape_markdown
+#from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, InlineQueryHandler
+#from telegram.utils.helpers import escape_markdown
 
 import base64
 import qrcode
@@ -108,7 +108,7 @@ def generateQR(message):
 
 # query handler ----------------------------------------------------------------------------------------------------------------------------------------------------
 
-@bot.callback_query_handler(func=lambda call: True)
+'''@bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
     bot.answer_callback_query(callback_query_id=call.id, text = "Answer processing...")
     answer = "You replied no.\nWhyyyyy :-("
@@ -118,79 +118,5 @@ def query_handler(call):
     bot.send_message(call.message.chat.id, answer)
     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 '''
-@bot.inline_handler(func=lambda query: True)
-def inlinequery(inline_query):
-    results = [InlineQueryResultArticle(
-        id = uuid4(),
-        title = '/datetime',
-        input_message_content = InputTextMessageContent(
-            message_text='/datetime'
-        ))]
-    bot.answer_inline_query(inline_query.id, results = results)
 
-def main():
-    updater = Updater(token, use_context = True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(InlineQueryHandler(inlinequery))
-    dp.add_error_handler(ValueError)
-    
-    updater.start_polling()
-    updater.idle()
-
-main()
-
-def inlinequery(update, context):
-    # Handle the inline query.
-    query = update.inline_query.query
-    results = [
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="/start",
-            input_message_content=InputTextMessageContent(
-                query.upper())),
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="/help",
-            input_message_content=InputTextMessageContent(
-                "*{}*".format(escape_markdown(query)),
-                parse_mode=ParseMode.MARKDOWN)),
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="/datetime",
-            input_message_content=InputTextMessageContent(
-                "_{}_".format(escape_markdown(query)),
-                parse_mode=ParseMode.MARKDOWN)),    
-        ]
-
-    update.inline_query.answer(results)
-
-def main():
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(token, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(InlineQueryHandler(inlinequery))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Block until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
-'''
 bot.polling()
